07a1c2f45f28879dbe598ee1100363eddd100f68
Thimerson <thimerson.ramirez7@gmail.com>
2025-10-27 00:21:28 +0100

    Add tests for data loading and transformation

---
+++ tests/test_data_processing.py
@@ -0,0 +1,10 @@
+# Test file for data processing functions
+import pytest
+
+def test_data_loading():
+    """Test data loading functionality"""
+    pass
+
+def test_data_transformation():
+    """Test data transformation functionality"""
+    pass
